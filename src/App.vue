b88c34670d5d72aff30c43ca8d6a59b9e6c6a65b
TotpalIstvan <totpal.istvan@petrik.hu>
2022-02-08 16:36:29 +0100

    szerkesztés működik hozzáadás nem

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
           <td>{{ statue.price }}</td>
           <td>
             <button @click="torles(statue.id)">Törlés</button>
-            <button>Szerkesztés</button>
+            <button @click="szerkesztes(statue.id)">Szerkesztés</button>
           </td>
        </tr>
        <tr>
@@ -30,8 +30,9 @@
      <label>Személy:</label><input type="text" v-model="statue.person"><br>
      <label>Magasság:</label><input type="number" v-model="statue.height"><br>
      <label>Ár:</label><input type="number" v-model="statue.price"><br>
-     <button @click="hozzaad()">Mentés</button>
-     <button>Mégse</button>
+     <button v-if="!szerkeszt"   @click="hozzaad" >Mentés</button>
+     <button v-if="szerkeszt"  @click="mentes" >Mentés</button>
+     <button @click="megse">Mégse</button>
      </form>
   </div>
 </template>
@@ -46,11 +47,13 @@ export default {
   },
     data() {
     return {
+      szerkeszt:false,
+      ment: false,
       statue: {
         id:null,
         person: "",
-        height: '',
-        price: ''
+        height: null,
+        price: null
       },
       statues: []
     }
@@ -70,8 +73,10 @@ export default {
     },
     
     async hozzaad() {
+      
       await fetch('http://127.0.0.1:8000/api/statues', {
-        method: 'POST', headers:{
+        method: 'POST', 
+        headers:{
             'Content-Type' : 'application/json',
             'Accept' : 'application/json' 
         },
@@ -79,7 +84,44 @@ export default {
       })
       await this.loadData()
       
-    }
+    },
+
+    async szerkesztes(id) {
+      this.szerkeszt = true
+      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`)
+     let data = await Response.json();
+     this.statue = {...data}
+    },
+
+    async reset() {
+      this.statue = {
+        id:null,
+        person: "",
+        height: null,
+        price: null
+      },
+    this.szerkesz4 = false
+    },
+    
+      async mentes() {
+         
+        await fetch(`http://127.0.0.1:8000/api/statues/${this.statue.id}`,
+        {
+          method: 'PATCH',
+          headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json' 
+          },
+          body: JSON.stringify(this.statue)
+        })
+         await this.loadData()
+         this.ment = false
+         this.reset()
+      
+      },
+    async megse() {
+        this.reset()
+      }
   },
   mounted() {
     this.loadData()
